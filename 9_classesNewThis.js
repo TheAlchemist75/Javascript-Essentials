dde4411f8a08d191f8e2ef3015044a140e223a76
TheAlchemist75 <shubhammanur0075@gmail.com>
2024-08-14 04:40:50 +0200

    Minor fix

---
+++ 9_classesNewThis.js
@@ -0,0 +1,8 @@
+//------------------------------New----------------------------------------------
+
+// The "new" keyword creates a new object
+
+const person = {}; // Standard way
+const person1 = new Object(); // Using the "new" keyword
+
+console.log(person, person1); // Output: {} {}
