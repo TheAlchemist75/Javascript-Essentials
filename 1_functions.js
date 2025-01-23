d731aba80516eb427f40d5e571483f46d5ea93d6
Shubham Manur <92208839+TheAlchemist75@users.noreply.github.com>
2025-01-23 16:23:51 +0100

    Update 1_functions.js

--- 1_functions.js
+++ 1_functions.js
@@ -23,5 +23,5 @@ const square1 = (number) => number*number;
 console.log(result1 = square(10));
 console.log(result2 = square1(10));
 
-//Same result, you can also remove brackets () if there is only one parameter
+//Same result, you can also remove brackets () if there is only one parameter and parenthesis {} if there is only one return statement
 //const square1 = number => number*number;
